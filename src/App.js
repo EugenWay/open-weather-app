60124d3ae9e87177400f4084013c1e5ccc7fbfbd
Eugene Way <eugeneway@MBP-Eugene.home>
2019-08-27 20:08:31 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -2,25 +2,60 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+ 
+    state = {
+
+        ip: '',
+        time: 1,
+        city: '',
+        temperature: '',
+        fetching: true
+
+    }
+
+
+    fetchWetherData = async ({city, ...data}) => {
+        console.log(city)
+        console.log(data)
+
+        const id = `90063b78cbe9ba03b7a25507256ba316`
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${id}`
+        
+        const response = await fetch(url)
+        const waData = await response.json()
+
+        console.log(waData)
+        
+    }
+
+    componentDidMount() {
+        this.fetchIP()
+    }
+
+    fetchIP = () => {
+        const apiKey = `6fb5855a4485fc870344d7200a3747b1`
+
+        fetch(`http://api.ipstack.com/check?access_key=${apiKey}`)
+                .then(response => response.json())
+                .then(data => this.fetchWetherData(data))
+        
+    }
+
+    render() {
+
+        return (
+        <div className="App">
+            <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                App for know weather in your city - 
+            </p>
+            
+            </header>
+        </div>
+        )
+    } 
 }
 
 export default App;
